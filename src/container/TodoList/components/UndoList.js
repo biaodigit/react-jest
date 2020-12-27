890cad2f7413c794093d64957ace3e4f9b9e8128
biaodigit <15622285015@163.com>
2020-12-27 22:25:58 +0800

    style:undo list

--- src/container/TodoList/components/UndoList.js
+++ src/container/TodoList/components/UndoList.js
@@ -10,12 +10,15 @@ class UndoList extends Component {
     render() {
         const { list, deleteItem } = this.props
         return (
-            <div>
-                <div data-test="count">{list.length}</div>
-                <ul>
+            <div className="undo-list">
+                <div className="undo-list-title">
+                    正在进行
+                    <div data-test="count" className="undo-list-count">{list.length}</div>
+                </div>
+                <ul className="undo-list-content">
                     {list.map((item, index) => (
-                        <li key={`${item}-${index}`} data-test="list-item">{item}
-                            <span onClick={() => deleteItem(index)} data-test="del-item">-</span></li>
+                        <li key={`${item}-${index}`} data-test="list-item" className="undo-list-item">{item}
+                            <span onClick={() => deleteItem(index)} data-test="del-item" className="undo-list-delete">-</span></li>
                     ))}
                 </ul>
             </div>
